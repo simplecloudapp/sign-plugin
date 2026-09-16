c1c7e444fe154a1c029676d296a559eaa3a6745d
fllipeis <p.eistrach@gmail.com>
2026-09-16 13:17:12 +0200

    fix: initialize Cloud item parser on Paper 26.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     alias(libs.plugins.shadow)
 }
 
-val baseVersion = "0.0.4"
+val baseVersion = "0.0.5"
 val commitHash = System.getenv("COMMIT_HASH")
 val snapshotversion = "${baseVersion}-dev.$commitHash"
 
